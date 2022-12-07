432c064ca0a6eab35a84e29fc8ecce7ceaefae2d
Erick-Fernandes-dev <erick.farias@dcx.ufpb.br>
2022-12-07 23:53:44 +0000

    Project POO

--- atv-2-3-cinthialindolfo/src/main/java/br/ufpb/dcx/rodrigor/vendasImpressora/Exemplo.java
+++ atv-2-3-cinthialindolfo/src/main/java/br/ufpb/dcx/rodrigor/vendasImpressora/Exemplo.java
@@ -5,6 +5,23 @@ public class Exemplo {
     public static void main(String[] argsadd){
         SistemaControleVendas controleVendas = new SistemaControleVendas();
 
+        //(1) PASSO
+        Pedido pedido = new Pedido();//jogar valores / parametros
+
+
+
+        //=========================================================
+
+        // (2) PASSO
+        // Scanner / JOptionPane (Inserir)
+        String opcaoImpressoa = "";
+
+        // =========================================================
+
+        // (3) PASSO
+        controleVendas.setImpressora(opcaoImpressoa, pedido);
+
+        
         // controleVendas.setImpressora(new ImpressoraEpson());
 
 
